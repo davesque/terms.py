91f6368b33314b8d5670d5c534e1e8697522e07f
David Sanders <davesque@gmail.com>
2015-10-18 14:41:53 -0600

    Updates

--- setup.py
+++ setup.py
@@ -8,4 +8,10 @@ setup(
     version=terms.version,
     packages=find_packages(),
     test_suite='terms.tests',
+    install_requires=[
+        'parsing==0.1',
+    ],
+    dependency_links=[
+        'https://github.com/davesque/parsing.py/tarball/master#egg=parsing-0.1',
+    ],
 )
